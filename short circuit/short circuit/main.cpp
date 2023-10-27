c194eea3c2c58f0ebddcdf636fd758a703130b92
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-10-27 22:39:51 +0800

    Add files via upload

---
+++ short circuit/short circuit/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  short circuit
+//
+//  Created by Nisha Ramprasath on 27/10/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a=5,b=10 , i=5;
+    if(a>b || ++i<=b){
+    }
+    cout<<i<<endl;
+    return 0;
+}
